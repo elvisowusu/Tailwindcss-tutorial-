5f31cc1397867efcd55c77c487c47f30ffa20d4a
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-23 15:34:32 +0000

    done with swithing theme with a button

--- src/Components/DarkMode/DarkModeWithSwitcher.jsx
+++ src/Components/DarkMode/DarkModeWithSwitcher.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 
 export default function DarkModeWithSwitcher (){
+
+    const [theme, setTheme] = useState("light");
+
+    useEffect(() => {
+        if(theme==="dark"){
+            //sets the document to darkMode when theme is dark
+            document.documentElement.classList.add("dark");
+        }else{
+            //removes the darkmode from the document when theme is light
+            document.documentElement.classList.remove("dark")
+        }
+    },[theme]); 
+
+    const handleThemeSwitcher =()=>{
+        setTheme(theme==="dark"? "light":"dark")
+    }
+
+
     return(
         <div className="flex items-center flex-col dark:bg-slate-500 h-[90vh]">
         <h1 className="mainHeading text-3xl text-center my-4 underline font-bold dark:text-slate-300">
           DARK MODE WHICH OCCURS WHEN A BUTTON IS CLICKED 
         </h1>
 
-        <button className="shadow-lg bg-slate-100 dark:bg-slate-800 dark:text-white px-6 py-2 rounded-lg">SWICH MODE</button>
+        <button onClick={handleThemeSwitcher} className="shadow-lg bg-slate-100 dark:bg-slate-800 dark:text-white px-6 py-2 rounded-lg">SWICH MODE</button>
 
         <p className="w-[38rem] dark:text-slate-200 mt-4">
         This feature works when darkMode : 'class', is set in the tailwind.config.js and when it's manually toggled 
