9e7ae33d5285131f539929806202887db77aa1f6
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-25 16:04:05 +0000

    Functions and Directives

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import DarkModeWithSwitcher from './Components/DarkMode/DarkModeWithSwitcher'
 import DarkModeWithSystemPreference from './Components/DarkMode/DarkModeWithSystemPreference'
 import DarkModeAllFeatures from './Components/DarkMode/DarkModeAllFeatures'
 import { ReusingStyles } from './Components/ReusingStyles'
-
+import { FunctionsAndDirectives } from './Components/Function&Directives'
 
 function App() {
 
   return (
     <div className='mb-[5rem]'>
-      <ReusingStyles/>
+      <FunctionsAndDirectives/>
     </div>
   )
 }
---
+++ src/Components/Function&Directives.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const FunctionsAndDirectives =()=>{
+    return(
+        <div className="flex gap-4 flex-col items-center">
+            <h1 className="text-[2rem] font-bold underline">FUNCTIONS AND DIRECTIVES</h1>
+        </div>
+    )
+}
