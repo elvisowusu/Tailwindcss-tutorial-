6f0e61123e06777ee065e26d0979b85347b244c2
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-25 13:03:34 +0000

    changes

--- src/Components/DarkMode/DarkModeWithSystemPreference.jsx
+++ src/Components/DarkMode/DarkModeWithSystemPreference.jsx
@@ -1,42 +1,42 @@
 import React, { useState, useEffect } from "react";
 
 export default function DarkModeWithSystemPreference() {
-//According to tailwindcss Docs
-    //   useEffect(() => {
-//     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-//     if (
-//       localStorage.theme === "dark" ||
-//       (!("theme" in localStorage) &&
-//         window.matchMedia("(prefers-color-scheme: dark)").matches)
-//     ) {
-//       document.documentElement.classList.add("dark");
-//     } else {
-//       document.documentElement.classList.remove("dark");
-//     }
+// According to tailwindcss Docs
+      useEffect(() => {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
 
-//     // Whenever the user explicitly chooses light mode
-//     localStorage.theme = "light";
+    // Whenever the user explicitly chooses light mode
+    localStorage.theme = "light";
 
-//     // Whenever the user explicitly chooses dark mode
-//     localStorage.theme = "dark";
+    // Whenever the user explicitly chooses dark mode
+    localStorage.theme = "dark";
 
-//     // Whenever the user explicitly chooses to respect the OS preference
-//     localStorage.removeItem("theme");
-//   }, []);
+    // Whenever the user explicitly chooses to respect the OS preference
+    localStorage.removeItem("theme");
+  }, []);
 
 
 // short form of the code above
-useEffect(() => {
-    // Check if the theme is set to "dark" in localStorage or if the user prefers dark mode based on their system settings
-    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    document.documentElement.classList.toggle("dark", localStorage.theme === "dark" || (!localStorage.theme && prefersDarkMode));
+// useEffect(() => {
+//     // Check if the theme is set to "dark" in localStorage or if the user prefers dark mode based on their system settings
+//     const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+//     document.documentElement.classList.toggle("dark", localStorage.theme === "dark" || (!localStorage.theme && prefersDarkMode));
 
-    // Cleanup function for useEffect
-    return () => {
-        // Remove the "theme" key from localStorage when the component unmounts
-        localStorage.removeItem("theme");
-    };
-}, []);
+//     // Cleanup function for useEffect
+//     return () => {
+//         // Remove the "theme" key from localStorage when the component unmounts
+//         localStorage.removeItem("theme");
+//     };
+// }, []);
 
 
   return (
