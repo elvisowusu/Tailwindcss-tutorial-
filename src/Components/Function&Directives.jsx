b934933f25e13a7b4c788599cd8746813d102a4f
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-28 13:19:50 +0000

    Refactor styles in FunctionsAndDirectives component

--- src/Components/Function&Directives.jsx
+++ src/Components/Function&Directives.jsx
@@ -4,7 +4,11 @@ import React from "react";
 export const FunctionsAndDirectives =()=>{
     return(
         <div className="flex gap-4 flex-col items-center">
-            <h1 className="text-[2rem] font-bold underline">FUNCTIONS AND DIRECTIVES</h1>
+            <h1 >FUNCTIONS AND DIRECTIVES</h1>
+            <p>The base styles are used as a general style of all the elements</p>
+            <p>It will have equal effect in this paragraph too and all other paragraphs</p>
+            <p className="paragraph">This applies a custom component styles to a particular component just like external css</p>        
+        
         </div>
     )
 }
