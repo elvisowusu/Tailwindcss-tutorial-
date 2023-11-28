8cb615a98d4537f06e2741bc84d95f2ba87bc773
elvisowusu <owusuelvisgyasi@gmail.com>
2023-11-28 13:43:01 +0000

    Add utility class for no-filter and update styles

--- src/Components/Function&Directives.jsx
+++ src/Components/Function&Directives.jsx
@@ -8,7 +8,7 @@ export const FunctionsAndDirectives =()=>{
             <p>The base styles are used as a general style of all the elements</p>
             <p>It will have equal effect in this paragraph too and all other paragraphs</p>
             <p className="paragraph">This applies a custom component styles to a particular component just like external css</p>        
-        
+            <p className="no-filter filter">@layer utility with no-filter</p>
         </div>
     )
 }
