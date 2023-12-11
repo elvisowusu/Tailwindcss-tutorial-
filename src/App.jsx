9e18d8424a874ce651c4dde362b4521b0f4ea5d2
elvisowusu <owusuelvisgyasi@gmail.com>
2023-12-11 14:08:05 +0000

    Finsihed Customization tutorials

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useState } from 'react'
-import image from './assets/sign In.png'
-import {AiOutlineArrowRight} from 'react-icons/ai'
-import './App.css'
-import {HoverFocusAndOtherStates} from './Components/CORE CONCEPTS/HoverFocusandOtherStates'
-import DarkMode from './Components/CORE CONCEPTS/DarkMode/DarkMode'
-import DarkModeWithSwitcher from './Components/CORE CONCEPTS/DarkMode/DarkModeWithSwitcher'
-import DarkModeWithSystemPreference from './Components/CORE CONCEPTS/DarkMode/DarkModeWithSystemPreference'
-import DarkModeAllFeatures from './Components/CORE CONCEPTS/DarkMode/DarkModeAllFeatures'
-import { ReusingStyles } from './Components/CORE CONCEPTS/ReusingStyles'
-import { FunctionsAndDirectives } from './Components/CORE CONCEPTS/Function&Directives'
-import { Configuration } from './Components/CUSTOMIZATION/Configuration'
+// import image from './assets/sign In.png'
+// import {AiOutlineArrowRight} from 'react-icons/ai'
+// import {HoverFocusAndOtherStates} from './Components/CORE CONCEPTS/HoverFocusandOtherStates'
+// import DarkMode from './Components/CORE CONCEPTS/DarkMode/DarkMode'
+// import DarkModeWithSwitcher from './Components/CORE CONCEPTS/DarkMode/DarkModeWithSwitcher'
+// import DarkModeWithSystemPreference from './Components/CORE CONCEPTS/DarkMode/DarkModeWithSystemPreference'
+// import DarkModeAllFeatures from './Components/CORE CONCEPTS/DarkMode/DarkModeAllFeatures'
+// import { ReusingStyles } from './Components/CORE CONCEPTS/ReusingStyles'
+// import { FunctionsAndDirectives } from './Components/CORE CONCEPTS/Function&Directives'
 
 function App() {
 
   return (
     <div className='mb-[5rem]'>
-      <Configuration />
     </div>
   )
 }
--- src/Components/CUSTOMIZATION/Configuration.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Configuration = () => {
-    return(
-        <div>
-            <h1>CONFIGURATION</h1>
-        </div>
-    )
-}
